c0eb2b04ca85914b4c291e7edd0216653cbb826f
Narayan Singh <narayansi@tumlare.com>
2025-06-04 18:29:14 +0530

    Continue on resolvers

--- server/index.js
+++ server/index.js
@@ -1,46 +1,61 @@
 const express = require("express");
-const {ApolloServer} = require("@apollo/server");
-const {expressMiddleware} = require("@apollo/server/express4");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { default: axios } = require("axios");
 
 async function startServer(){
     const app = express();
+    const PORT = 4000;
     const server = new ApolloServer({
          typeDefs: `
+            type User {
+                id: ID!
+                name: String!
+                username: String!
+                email: String!
+                phone: String!
+                website: String!
+            }
             type Todo {
                 id: ID!
                 title: String!
                 completed: Boolean
+                user: User
             }
 
             type Query {
                 getTodos: [Todo]
+                getAllUsers: [User]
+                getUser(id: ID!): User
             }
         `,
         resolvers: {
-           /*  Query: {
-                hello: () => "Hello world!"
-            } */
+            Todo: {
+                user: async (todo) => 
+                    (
+                        await axios.get(`https://jsonplaceholder.typicode.com/users/${todo.userId}`)).data
+            },
+            Query: {
+                getTodos: async () => (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
+                getAllUsers: async () => (await axios.get('https://jsonplaceholder.typicode.com/users')).data,
+                getUser: async (parent, { id }) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`)).data,  
+            }
         }
     });
 
-    app.use(bodyParser.json());
-    app.use(cors());
-
     await server.start();
 
-    // app.use("/graphql", expressMiddleware(server));
-    app.use("/graphql", expressMiddleware(server, {
-        context: ({ req }) => ({ req }),
-    }));
+    app.use(express.json());
+    app.use(cors());
 
-    app.listen(8000, () => {
-        console.log("Server is running on http://localhost:8000/graphql");
+
+    app.use("/graphql", expressMiddleware(server));
+
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}/graphql`);
     });
 }
 
-startServer().catch(err => {
-    console.error("Failed to start server:", err);
-    process.exit(1);
-});
+startServer();
