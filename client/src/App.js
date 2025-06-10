87cdce5bfaf7a34144da699d116b1811c49ba39a
Mr. Samy <learnsamy@gmail.com>
2025-06-10 21:01:29 +0530

    Display the data in the tabular form

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,50 @@
 import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+
+const query = gql`
+  query GetTodosWithUsers {
+    getTodos {
+      title
+      completed
+      user {
+        name
+        email
+        phone
+      }
+    }
+  }`;
 
 function App() {
+  const { data, loading  } = useQuery(query);
+
+  if (loading) return <p>Loading...</p>;
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className='App'> 
+      {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+      <table border={1} cellPadding={10} cellSpacing={0}>
+        <tbody>
+          <tr>
+            <th>S.No</th>
+            <th>Title</th>
+            <th>Completed</th>
+            <th>User Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+          </tr>
+          {data.getTodos.map((todo, index) => (
+            <tr key={index}>
+              <td>{index}</td>
+              <td>{todo.title}</td>
+              <td>{todo.completed ? 'Yes' : 'No'}</td>
+              <td>{todo.user.name}</td>
+              <td>{todo.user.email}</td>
+              <td>{todo.user.phone}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
-ReactDOM.render(
+const client  = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
 );
